54a2112794adc6e8f36a2ba12e1e93933389cdff
BB-ACK <84366169+BB-ACK@users.noreply.github.com>
2025-09-15 13:06:20 +0900

    [level 2] Title: 올바른 괄호, Time: 0.63 ms, Memory: 3.85 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -0,0 +1,37 @@
+#include<string>
+#include <iostream>
+#include <stack>
+
+using namespace std;
+
+bool solution(string s)
+{
+    bool answer = true;
+    stack<char> stack;
+    
+    
+    for(int i = 0; i < s.length(); i++) {
+        if(s[i] == '(') 
+            stack.push(s[i]);
+        else if(s[i] == ')'){
+            if(stack.empty()) {
+                answer = false;
+                break;
+            }
+            
+            char top = stack.top();
+            if(top == '(') {
+                stack.pop();
+            }
+            else {
+                answer = false;
+                break;
+            }
+        }
+    }
+    
+    if(!stack.empty())
+        answer = false;
+    
+    return answer;
+}
