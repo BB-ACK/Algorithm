75e11df39480b47a1a9bd024532a1e8e35f304ab
BB-ACK <84366169+BB-ACK@users.noreply.github.com>
2026-03-31 00:00:08 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.cc
+++ 백준/Bronze/15829. Hashing/Hashing.cc
@@ -1,26 +1,36 @@
 #include <iostream>
 #include <string>
-#include <cmath>
+
 using namespace std;
 
-int hashFn(string word) {
-    int r = 31;
-    int result = 0;
+int main() {
+    // 빠른 입출력을 위한 설정
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
 
-    for(int i = 0; i < word.length(); i++) {
-        int tmp = word[i] - 'a' + 1;
-        result += tmp * pow(r, i);
+    int L;
+    cin >> L;
+    string s;
+    cin >> s;
+
+    long long sum = 0;
+    long long r_pow = 1; // r^i 값을 저장 (처음은 31^0 = 1)
+    long long M = 1234567891;
+    int r = 31;
+
+    for (int i = 0; i < L; i++) {
+        // 1. 문자를 숫자로 변환 (a=1, b=2, ...)
+        int value = s[i] - 'a' + 1;
+
+        // 2. 항을 계산하여 더함 (중간중간 M으로 나머지 연산 필수)
+        // (value * r^i) % M 을 구해서 더함
+        sum = (sum + (value * r_pow)) % M;
+
+        // 3. 다음 항을 위해 r^i에 r을 곱함 (여기서도 M으로 나머지 연산)
+        r_pow = (r_pow * r) % M;
     }
 
-    return result;
-}
-
-
-int main() {
-    int len; cin >> len;
-    string word; cin >> word;
-
-    cout << hashFn(word);
+    cout << sum;
 
     return 0;
 }
